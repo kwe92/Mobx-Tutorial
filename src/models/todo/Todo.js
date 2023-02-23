ca9aefadc92bc0c9adef46b5b57f6cd6b49ec8b5
Kweayon  Clark <Kweayon@gmail.com>
2023-02-23 16:02:02 -0600

    More Notes && Tests

--- src/models/todo/Todo.js
+++ src/models/todo/Todo.js
@@ -7,30 +7,33 @@ var mobx_state_tree_1 = require("mobx-state-tree");
 // MobX Tree (Living Tree - Mutable State)
 //   1. Mutable state
 //   2. Protected strictly typed objects (run-time type information)
-//   3. The tree has:
-//      3.1: shape (type information)
-//      3.2: state (data)
+//   3. Tree Model (The Abstraction):
+//      3.1: Shape (model, type information, views, actions) {}
+//      3.2: Instance state (data) / (Instantiation of the Model)
 // MobX Tree snapshots
 //   1. Generated automatically
 //   2. Immutable and structurally shared snapshots are generated automaticallys
+// MobX: treeNode = types.model({}).views({}).actions({});
+//   1. The instantiation of a Tree Model
+//   2. How MST defines its interfaces / classes
+//   3. Main Methods:
+//   3.1: .model({}) = Member variable type definitions
+//   3.2: .views({}) = Derivation processes | get getter computed variable / value as a derivative of state that typically returns a value
+//   3.3: .actions({}) = Set state actions Methods / Functions | Ussually does not return a value
 // Summary:
 //   1. MST is MobX with type saftey and the use of interfaces for entities instead of classes
 //   2. Manage state outside of the view (Conmponent) and without the need of a class entity
 //   3. Has a strange syntaxs at first forsure
-// MobX: entity = types.model({})
-//   1. interface / type  / class / datatype
-//   2. How MST defines its interfaces
-//   3. member variable type definitions
 // Create two entities:
 //   1. Todo
 //   2. Author
-// Tree Model
+// Tree Model | The Abstraction / Mix of Interface and Implementation
 var Author = mobx_state_tree_1.types.model({
     id: mobx_state_tree_1.types.identifier,
     fname: mobx_state_tree_1.types.string,
-    lname: mobx_state_tree_1.types.string
+    lname: mobx_state_tree_1.types.optional(mobx_state_tree_1.types.string, "")
 });
-// Tree Model
+// Tree Model| The Abstraction / Mix of Interface and Implementation
 var Todo = mobx_state_tree_1.types.model({
     id: mobx_state_tree_1.types.identifier,
     task: mobx_state_tree_1.types.string,
@@ -40,10 +43,11 @@ var Todo = mobx_state_tree_1.types.model({
 // Author Object: Passed to Author.create()
 var authorObject = {
     id: "1101",
-    fname: "Kweayon",
-    lname: "Clark"
+    fname: "Kweayon"
 };
+// Tree Node | The Instantiation
 var auth1 = Author.create(authorObject);
+// Tree Node | The Instantiation
 var todo1 = Todo.create({
     id: "1101",
     task: "Finish first MobX State Tree tutorial!",
--- src/models/todo/Todo.tsx
+++ src/models/todo/Todo.tsx
@@ -1,27 +1,37 @@
 import { types, getSnapshot } from "mobx-state-tree";
 
 // Entities and Their Many Names
+
 //   1. entity / type / interface / class / object / | set theory discrete mathematics
 //   2. They are all pretty similar in theory and concept
 
 // MobX Tree (Living Tree - Mutable State)
+
 //   1. Mutable state
 //   2. Protected strictly typed objects (run-time type information)
-//   3. The tree has:
+//   3. Tree Model (The Abstraction):
 //      3.1: Shape (model, type information, views, actions) {}
-//      3.2: Instance state (data) || (Instantiated)
+//      3.2: Instance state (data) / (Instantiation of the Model)
 
 // MobX Tree snapshots
+
 //   1. Generated automatically
 //   2. Immutable and structurally shared snapshots are generated automaticallys
 
 // MobX: treeNode = types.model({}).views({}).actions({});
-//   1. interface / type  / class / datatype
+
+//   1. The instantiation of a Tree Model
 //   2. How MST defines its interfaces / classes
 //   3. Main Methods:
 //   3.1: .model({}) = Member variable type definitions
-//   3.2: .views({}) = Derivation processes | get getter computed variable / value as a derivative of state
-//   3.3:
+//   3.2: .views({}) = Derivation processes | get getter computed variable / value as a derivative of state that typically returns a value
+//   3.3: .actions({}) = Set state actions Methods / Functions | Ussually does not return a value
+
+// types Namespace
+//   - A namespace of types for MobX and Typescript
+
+// Null saftey (types.optional)
+//   - types.optional(types.typeDeclaration, defaultValue);
 
 // Summary:
 //   1. MST is MobX with type saftey and the use of interfaces for entities instead of classes
@@ -32,14 +42,14 @@ import { types, getSnapshot } from "mobx-state-tree";
 //   1. Todo
 //   2. Author
 
-// Tree Model
+// Tree Model | The Abstraction / Mix of Interface and Implementation
 const Author = types.model({
   id: types.identifier,
   fname: types.string,
-  lname: types.string,
+  lname: types.optional(types.string, ""),
 });
 
-// Tree Model
+// Tree Model| The Abstraction / Mix of Interface and Implementation
 const Todo = types.model({
   id: types.identifier,
   task: types.string,
@@ -51,11 +61,12 @@ const Todo = types.model({
 const authorObject = {
   id: "1101",
   fname: "Kweayon",
-  lname: "Clark",
 };
 
+// Tree Node | The Instantiation
 const auth1 = Author.create(authorObject);
 
+// Tree Node | The Instantiation
 const todo1 = Todo.create({
   id: "1101",
   task: "Finish first MobX State Tree tutorial!",
