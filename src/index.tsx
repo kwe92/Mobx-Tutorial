5c17a98261c1b6df49ade766778f377f6ec0afe8
Kweayon  Clark <Kweayon@gmail.com>
2023-02-24 12:53:03 -0600

    Added observer

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AppGlobalTheme } from "./indexStyles";
+import CountStore from "./models/count/Count";
+import { applySnapshot } from "mobx-state-tree";
+
+const store = CountStore.create({ count: { "1001": { id: 1001, count: 0 } } });
+
+// applySnapshot(store, {});
+
+// store.addCount({ id: 1001, count: 0 });
+
 const ele = document.getElementById("root") as HTMLElement;
 
 const root = ReactDOM.createRoot(ele);
@@ -9,6 +18,6 @@ const root = ReactDOM.createRoot(ele);
 root.render(
   <React.StrictMode>
     <AppGlobalTheme />
-    <App />
+    <App store={store} />
   </React.StrictMode>
 );
