f8a0873cde714e8281489c589ee1937a95aa2331
Kweayon  Clark <Kweayon@gmail.com>
2023-02-24 12:52:15 -0600

    Added MobX State Management

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,20 +1,35 @@
-import React from "react";
+import { values } from "mobx";
+import { observer } from "mobx-react-lite";
+import React, { useEffect, useState } from "react";
 import {
   MainContainer,
   CounterContainer,
   StyledButton,
   Count,
 } from "./CounterStyles";
-const Counter = (props: {}) => {
+import CountStore from "../../models/count/Count";
+const Counter = observer((props: { store: any }) => {
+  const handleIncrement = () => {
+    props.store.count.get("1001")!.increment();
+  };
+
+  const handleDecrement = () => {
+    props.store.count.get("1001")!.decrement();
+  };
+
+  console.log("store: ", props.store.count.get("1001").count);
   return (
     <MainContainer>
       <CounterContainer>
-        <Count>0</Count>
-        <StyledButton>Increment</StyledButton>
-        <StyledButton>Decrement</StyledButton>
+        <Count>
+          {/* {count} */}
+          {props.store.count.get("1001").count}
+        </Count>
+        <StyledButton onClick={handleIncrement}>Increment</StyledButton>
+        <StyledButton onClick={handleDecrement}>Decrement</StyledButton>
       </CounterContainer>
     </MainContainer>
   );
-};
+});
 
 export default Counter;
