26bc252625f6ae25426482ef72e3958705253e72
Kweayon  Clark <Kweayon@gmail.com>
2023-02-24 10:03:06 -0600

    Added Global Styling

---
+++ src/indexStyles.tsx
@@ -0,0 +1,19 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+const AppGlobalTheme = createGlobalStyle`
+
+  * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+ body{}
+
+    li {
+        line-style: none;
+    }
+
+`;
+
+export { AppGlobalTheme }
